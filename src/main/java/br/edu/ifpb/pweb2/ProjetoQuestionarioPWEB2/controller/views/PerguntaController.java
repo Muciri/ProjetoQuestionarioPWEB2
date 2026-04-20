47173a378b4e6bfd858a82145782ef8c7a12d1f5
Murilo Maciel Rodrigues <murilomacielbr@gmail.com>
2026-04-20 12:10:10 -0300

    feat: adiciona fucionalidades de cadastro e edição de perguntas

--- src/main/java/br/edu/ifpb/pweb2/ProjetoQuestionarioPWEB2/controller/views/PerguntaController.java
+++ src/main/java/br/edu/ifpb/pweb2/ProjetoQuestionarioPWEB2/controller/views/PerguntaController.java
@@ -2,27 +2,103 @@ package br.edu.ifpb.pweb2.ProjetoQuestionarioPWEB2.controller.views;
 
 import br.edu.ifpb.pweb2.ProjetoQuestionarioPWEB2.model.Pergunta;
 import br.edu.ifpb.pweb2.ProjetoQuestionarioPWEB2.repository.PerguntaRepository;
+import br.edu.ifpb.pweb2.ProjetoQuestionarioPWEB2.service.PerguntaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
+@RequestMapping("/perguntas")
 public class PerguntaController {
+    private final PerguntaRepository perguntaRepository;
+    private final PerguntaService perguntaService;
 
     @Autowired
-    private PerguntaRepository perguntaRepository;
+    public PerguntaController(PerguntaRepository perguntaRepository, PerguntaService perguntaService) {
+        this.perguntaRepository = perguntaRepository;
+        this.perguntaService = perguntaService;
+    }
 
-    @GetMapping("/perguntas")
-    public String listarPerguntas(Model model) {
-        List<Pergunta> perguntas = perguntaRepository.findAll();
+    // galera, coloquei esse método aqui por que ainda não tem feito o lance do admin e tal
+    // depois de implementar a autenticação e interceptadores, vou refatorar este código, tirar a verificação repetida
+    private boolean isUsuarioAdmin() {
+        return true;
+    }
+    
+    @GetMapping("")
+    public ModelAndView listarPerguntas(ModelAndView model, RedirectAttributes redirectAttributes) {
+        if (!isUsuarioAdmin()) {
+            redirectAttributes.addFlashAttribute("erro", "acesso negado");
+            return new ModelAndView("redirect:/perguntas");
+        }
 
-        model.addAttribute("titulo", "Lista de Perguntas");
-        model.addAttribute("mensagem", "Perguntas cadastradas no sistema");
-        model.addAttribute("perguntas", perguntas);
+        model.setViewName("perguntas/listaPerguntas");
+        model.addObject("titulo", "Lista de Perguntas");
+        model.addObject("perguntas", perguntaService.getPerguntas());
+        model.addObject("isAdmin", isUsuarioAdmin());
 
-        return "perguntas/listaPerguntas";
+        return model;
+    }
+
+    @GetMapping("/form")
+    public ModelAndView exibirFormularioCadastro(ModelAndView model, RedirectAttributes redirectAttributes) {
+        if (!isUsuarioAdmin()) {
+            redirectAttributes.addFlashAttribute("erro", "acesso negado");
+            return new ModelAndView("redirect:/perguntas");
+        }
+
+        model.setViewName("perguntas/formularioPergunta");
+        model.addObject("pergunta", new Pergunta());
+        model.addObject("titulo", "Nova Pergunta");
+        return model;
+    }
+
+    @PostMapping("/salvar")
+    public ModelAndView salvarPergunta(@ModelAttribute("pergunta") Pergunta pergunta, RedirectAttributes redirectAttributes) {
+        if (!isUsuarioAdmin()) {
+            redirectAttributes.addFlashAttribute("erro", "acesso negado");
+            return new ModelAndView("redirect:/perguntas");
+        }
+
+        if (pergunta.getId() != null) {
+            perguntaService.UpdatePergunta(pergunta.getId(), pergunta);
+        } else {
+            perguntaService.createPergunta(pergunta);
+        }
+
+        redirectAttributes.addFlashAttribute("mensagem", "pergunta salva com sucesso!");
+        return new ModelAndView("redirect:/perguntas");
+    }
+
+    @GetMapping("/editar/{id}")
+    public ModelAndView exibirFormularioEdicao(@PathVariable Long id,ModelAndView model, RedirectAttributes redirectAttributes) {
+        if (!isUsuarioAdmin()) {
+            redirectAttributes.addFlashAttribute("erro", "acesso negado");
+            return new ModelAndView("redirect:/perguntas");
+        }
+
+        model.setViewName("perguntas/formularioPergunta");
+        Pergunta pergunta = perguntaService.getPerguntaById(id);
+
+        model.addObject("pergunta", pergunta);
+        model.addObject("titulo", "Editar Pergunta");
+
+        redirectAttributes.addFlashAttribute("mensagem", "pergunta salva com sucesso!");
+        return model;
+    }
+
+    @GetMapping("/excluir/{id}")
+    public ModelAndView excluirPergunta(@PathVariable Long id, RedirectAttributes redirectAttributes) {
+        if (!isUsuarioAdmin()) {
+            redirectAttributes.addFlashAttribute("erro", "acesso negado");
+            return new ModelAndView("redirect:/perguntas");
+        }
+
+        perguntaService.deletePerguntaById(id);
+
+        redirectAttributes.addFlashAttribute("mensagem", "pergunta excluída com sucesso!");
+        return new ModelAndView("redirect:/perguntas");
     }
 }
