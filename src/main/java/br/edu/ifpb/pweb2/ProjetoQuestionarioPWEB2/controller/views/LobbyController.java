f1297cdc111840d728eeedb72cab02199dc0bf6a
fvmaia <upciga.app@gmail.com>
2026-06-22 16:53:23 -0300

    fix: corrige marcacao de corridas concluidas no lobby apos migracao para spring security

--- src/main/java/br/edu/ifpb/pweb2/ProjetoQuestionarioPWEB2/controller/views/LobbyController.java
+++ src/main/java/br/edu/ifpb/pweb2/ProjetoQuestionarioPWEB2/controller/views/LobbyController.java
@@ -9,9 +9,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import br.edu.ifpb.pweb2.ProjetoQuestionarioPWEB2.model.Corrida;
 import br.edu.ifpb.pweb2.ProjetoQuestionarioPWEB2.model.Participante;
 import br.edu.ifpb.pweb2.ProjetoQuestionarioPWEB2.service.CorridaService;
+import br.edu.ifpb.pweb2.ProjetoQuestionarioPWEB2.service.ParticipanteService;
 import br.edu.ifpb.pweb2.ProjetoQuestionarioPWEB2.service.ResultadoService;
 import br.edu.ifpb.pweb2.ProjetoQuestionarioPWEB2.service.SessaoCorridaService;
 import jakarta.servlet.http.HttpSession;
+
+import java.security.Principal;
 
 @Controller
 @RequestMapping("/lobby")
@@ -20,17 +23,19 @@ public class LobbyController {
     private final CorridaService corridaService;
     private final ResultadoService resultadoService;
     private final SessaoCorridaService sessaoCorridaService;
+    private final ParticipanteService participanteService;
 
-    public LobbyController(CorridaService corridaService, ResultadoService resultadoService, SessaoCorridaService sessaoCorridaService) {
+    public LobbyController(CorridaService corridaService, ResultadoService resultadoService, SessaoCorridaService sessaoCorridaService, ParticipanteService participanteService) {
         this.corridaService = corridaService;
         this.resultadoService = resultadoService;
         this.sessaoCorridaService = sessaoCorridaService;
+        this.participanteService = participanteService;
     }
 
     @GetMapping
     @PreAuthorize("hasAnyRole('PARTICIPANTE', 'ADMIN')")
-    public String lobby(HttpSession session, Model model) {
-        Participante participante = (Participante) session.getAttribute("participante");
+    public String lobby(HttpSession session, Model model, Principal principal) {
+        Participante participante = participanteService.findByNome(principal.getName());
 
         //pausa o cronometro qnd sair da corrida
         sessaoCorridaService.pausar(session);
